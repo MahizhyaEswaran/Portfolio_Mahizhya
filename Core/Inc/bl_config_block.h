889f52f55a93e683832ba212c2b4c18703ecb368
Anistus_H <anistush@gmail.com>
2023-08-07 17:12:50 +0530

    feat: add bootloader conf block files

---
+++ Core/Inc/bl_config_block.h
@@ -0,0 +1,38 @@
+/*
+ * Configuration block to be used by Bootloader
+ * And the user application
+ * By: Anistus H
+ * 2023/07/13
+ */
+
+#ifndef INC_BL_CONFIG_BLOCK_H_
+#define INC_BL_CONFIG_BLOCK_H_
+
+#include <string.h>
+#include <flash.h>
+
+#define BL_Config_Address 0x08007C00
+
+typedef struct BL_ConfigBlock {
+	uint8_t block_start;
+    char url[80];
+    char apn[10];
+    uint8_t bl_flag;
+    uint8_t is_fw_available;
+    uint8_t is_fw_downloaded;
+    uint8_t is_fw_varified;
+    uint8_t is_fw_copied;
+    uint32_t bl_version;
+    uint32_t current_fw_version;
+    uint32_t fw_size;
+    uint8_t is_error_occurred;
+    uint8_t error_state;
+    uint8_t remaining_tries;
+    uint8_t block_end;
+} BL_Config_Block;
+
+int bl_config_block_init(BL_Config_Block *q);
+uint8_t save_bl_config_block(BL_Config_Block *q);
+uint8_t read_bl_config_block(BL_Config_Block *q);
+
+#endif /* INC_BL_CONFIG_BLOCK_H_ */
---
+++ Core/Src/bl_config_block.c
@@ -0,0 +1,37 @@
+/*
+ * Configuration block to be used by Bootloader
+ * And the user application
+ * By: Anistus H
+ * 2023/07/13
+ */
+
+#include <bl_config_block.h>
+
+// Initialize the config block
+int bl_config_block_init(BL_Config_Block *q) {
+	read_bl_config_block(q);
+	if((q->block_start == 8) && (q->block_end == 17)){
+		//Already available
+		return 1;
+	}else{
+		//Not available
+		memset(q, 0 , sizeof(BL_Config_Block));
+		q->block_start = 8;
+		q->block_end = 17;
+		save_bl_config_block(q);
+		return 2;
+	}
+}
+
+// Store config block to flash
+uint8_t save_bl_config_block(BL_Config_Block *q) {
+	Flash_Erase_Page(BL_Config_Address, 2);
+    Flash_Write_Data(BL_Config_Address , (uint32_t *) q, sizeof(BL_Config_Block));
+    return 1;
+}
+
+// Read config block from flash
+uint8_t read_bl_config_block(BL_Config_Block *q) {
+	memcpy(q, (void *) BL_Config_Address , sizeof(BL_Config_Block));
+	return 1;
+}
